bc7758ded48d60a60fdf5ff87416fcf1b587052d
hehehwangPC <hehehwang@gmail.com>
2021-01-30 00:57:25 +0900

    cpp & python: setups

--- pythonStudy/solving.py
+++ pythonStudy/solving.py
@@ -1,3 +1,3 @@
-# 10871 x보다 작은 수
-n, x = map(int, input().split())
-[print(i, end=' ') for i in map(int, input().split()) if i < x]
+#
+input = __import__('sys').stdin.readline
+
--- pythonStudy/solvingPC.py
+++
@@ -1,1 +0,0 @@
-# pycharm commit&push test
