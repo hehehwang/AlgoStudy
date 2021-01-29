439f5687a98b1468a817defba3837dd7a4f51876
hehehwangPC <hehehwang@gmail.com>
2021-01-30 00:47:23 +0900

    cpp: update basic settings

--- cppStudy/algoScratch/algoScratch.cpp
+++ cppStudy/algoScratch/algoScratch.cpp
@@ -1,19 +1,13 @@
-﻿// 2480 주사위 세개
-#include <bits/stdc++.h>
+﻿#include <bits/stdc++.h>
 using namespace std;
+// 
+#define FOR(i, n, m) for(int i=(n); i<(m); i++)
+#define X first
+#define Y second
 
-int main()
-{
+int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int v[3];
-	cin >> v[0] >> v[1] >> v[2];
-	if (v[0] == v[1] && v[1] == v[2])
-		cout << (10000 + v[0] * 1000);
-	else if ( (v[0] == v[1]) || (v[0] == v[2]))
-		cout << 1000 + 100 * v[0];
-	else if (v[1] == v[2])
-		cout << 1000 + 100 * v[1];
-	else
-		cout<< max(max(v[0], v[1]), v[2])*100;
+	
+
 }
