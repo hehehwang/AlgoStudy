c687394a0f21f47d24639e0f199e21253e1f1682
hehehwangPC <hehehwang@gmail.com>
2021-04-06 05:36:03 +0900

    modified snippet + a

--- cppStudy/algoScratch/algoScratch.cpp
+++ cppStudy/algoScratch/algoScratch.cpp
@@ -1,11 +1,11 @@
 ﻿#include <bits/stdc++.h>
-using namespace std;
 // 
 #define FOR(i, n, m) for(int i=(n); i<(m); i++)
+using namespace std;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
-
+	
 }
