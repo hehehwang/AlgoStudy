dc28e391aa0e20dd1aaca4fdf14fa6db2231e94b
hehehwangPC <hehehwang@gmail.com>
2021-04-08 05:49:44 +0900

    cpp: 7576 토마토

--- cppStudy/Solved/BOJ/7576 토마토.cpp
+++ cppStudy/Solved/BOJ/7576 토마토.cpp
@@ -1,59 +1,57 @@
 #include <bits/stdc++.h>
-using namespace std;
 // 7576 토마토
+// https://www.acmicpc.net/problem/7576
+#define FOR(i, n, m) for(int i=(n); i<(m); i++)
+using namespace std;
 
-int h, w;
+int R, C;
 int board[1002][1002];
 int vis[1002][1002];
-int dy[4] = { 0, 0, 1, -1 };
-int dx[4] = { 1, -1, 0, 0 };
 queue<pair<int, int>> Q;
+int main(void)
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+   cin >> C >> R;
+   FOR(i, 0, R) {
+      FOR(j, 0, C) {
+         cin >> board[i][j];
+         if (board[i][j] == 1) {
+            Q.push({ i, j });
+            vis[i][j] = 1;
+         }
+         else if (!board[i][j]) vis[i][j] = -1;
+      }
+   }
 
-	cin >> w >> h;
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 1) {
-				Q.push({ i, j });
-				vis[i][j] = 1;
-			}
-			else if (board[i][j] == 0) vis[i][j] = -1;
-		}
-	}
-
-	while (!Q.empty()) {
-		pair<int, int> v = Q.front(); Q.pop();
-		for (int i = 0; i < 4; i++) {
-			int ny = v.first + dy[i];
-			int nx = v.second + dx[i];
-			if (ny < 0 || h <= ny || nx < 0 || w <= nx) continue;
-			if (vis[ny][nx] >= 0) continue;
-			vis[ny][nx] = vis[v.first][v.second] + 1;
-			Q.push({ ny, nx });
-		}
-	}
-	bool F = true;
-	int mxVis = 0;
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			if (vis[i][j] == -1) {
-				F = false;
-				break;
-			}
-			mxVis = max(mxVis, vis[i][j]);
-		}
-	}
-	//for (int i = 0; i < h; i++) {
-	//	for (int j = 0; j < w; j++) {
-	//		cout << vis[i][j] << ' ';
-	//	}
-	//	cout << "\n";
-	//}
-	if (F) cout << mxVis - 1;
-	else cout << -1;
+   while (!Q.empty()) {
+      int vr, vc;
+      tie(vr, vc) = Q.front();
+      Q.pop();
+      FOR(k, 0, 4) {
+         int nr = vr + "1012"[k] - '1';
+         int nc = vc + "2101"[k] - '1';
+         if (nr < 0 || nc < 0 || R <= nr || C <= nc)
+            continue;
+         if (0 <= vis[nr][nc]) continue;
+         vis[nr][nc] = vis[vr][vc] + 1;
+         Q.push({ nr, nc });
+      }
+   }
+   bool F = true;
+   int mxVis = 0;
+   FOR(i, 0, R) {
+      FOR(j, 0, C) {
+         if (vis[i][j] == -1) {
+            F = false;
+            break;
+         }
+         mxVis = max(mxVis, vis[i][j]);
+      }
+   }
+   if (F)
+      cout << mxVis - 1;
+   else
+      cout << -1;
 }
-
