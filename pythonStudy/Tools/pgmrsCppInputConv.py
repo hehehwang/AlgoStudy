0d966408c584f0b087f8245bcd42e0effd3215ba
heheHwang <hehehwang@gmail.com>
2021-05-30 23:23:08 +0900

    remove some files

--- pythonStudy/Tools/pgmrsCppInputConv.py
+++
@@ -1,8 +0,0 @@
-#
-input = __import__('sys').stdin.readline
-
-ss = input().strip()
-for s in ss:
-    if s == '[': print('{', end='')
-    elif s == ']': print('}', end='')
-    else: print(s, end='')
