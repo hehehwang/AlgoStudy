e55912e883fcc01100eb1e3fa79ff37cfc1c5f6e
heheHwang <hehehwang@gmail.com>
2021-07-11 15:39:33 +0900

    add 11780 플로이드 2.py

--- pythonStudy/Solved/BOJ/11780 플로이드 2.py
+++ pythonStudy/Solved/BOJ/11780 플로이드 2.py
@@ -5,12 +5,12 @@ n, m = int(input()), int(input())
 n += 1
 mxN = 10**8+1
 floyd = [[mxN]*(n) for _ in range(n)]
-enroute = [[0]*(n) for _ in range(n)]
+route = [[0]*(n) for _ in range(n)]
 
 for _ in range(m):
     a, b, c = map(int, input().split())
     floyd[a][b] = min(floyd[a][b], c)
-    enroute[a][b] = b
+    route[a][b] = b
 
 for k in range(1, n):
     for i in range(1, n):
@@ -21,22 +21,24 @@ for k in range(1, n):
             bypass = floyd[i][k] + floyd[k][j]
             if bypass < floyd[i][j]:
                 floyd[i][j] = bypass
-                enroute[i][j] = enroute[i][k]
-            # floyd[i][j] = min(floyd[i][j], floyd[i][k] + floyd[k][j])
+                route[i][j] = route[i][k]
+
 for i in range(n):
     for j in range(n):
         if floyd[i][j] == mxN: floyd[i][j] = 0
+
 [print(' '.join(map(str, f[1:]))) for f in floyd[1:]]
-# print()
-# [print(' '.join(map(str, f[1:]))) for f in enroute[1:]]
-# print()
 for i in range(1, n):
     for j in range(1, n):
         if i==j:
             print(0)
             continue
-        route = [i]
-        while route[-1] != j:
-            route.append(enroute[route[-1]][j])
-        print(len(route), end=' ')
-        print(' '.join(map(str, route)))
+        nxt = [i]
+        while 1:
+            if nxt[-1] == j:
+                print(len(nxt), ' '.join(map(str, nxt)))
+                break
+            elif nxt[-1] == 0:
+                print(0)
+                break
+            nxt.append(route[nxt[-1]][j])
