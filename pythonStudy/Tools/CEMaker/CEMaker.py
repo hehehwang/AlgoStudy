881782e1cd9a88d504c8bbe6716933726015d56c
heheHwang <hehehwang@gmail.com>
2021-07-17 18:51:14 +0900

    feature CEMaker.py, inputGenerator.py problem: SCPC2021_2

--- pythonStudy/Tools/CEMaker/CEMaker.py
+++ pythonStudy/Tools/CEMaker/CEMaker.py
@@ -19,7 +19,7 @@ def processCe(inp, ans, prob):
     print("WA:")
     print(prob)
 
-def main():
+def main_p1799():
     ceFound = False
     idx = 1
     for inp in p1799(5):
@@ -29,6 +29,28 @@ def main():
         ans, prob = loop.run_until_complete(commands)
         print("Case No.", idx)
         idx += 1
+        if ans != prob:
+            ceFound = True
+            processCe(inp, ans, prob)
+            loop.close()
+            break
+
+    print("NO CE FOUND T_T") if not ceFound else print('CE FOUND!')
+
+def main():
+    ceFound = False
+    idx = 1
+    gen = scpc2021_2()
+    for inp in gen:
+        loop = asyncio.get_event_loop()
+        print("Case No.", idx)
+        commands = asyncio.gather(runCmd(['./exec/2-naive.exe'], inp), # ['python', './prob.py']
+                                  runCmd(['./exec/2.exe'], inp))
+        ans, prob = loop.run_until_complete(commands)
+        idx += 1
+        print(inp)
+        print(ans)
+        print()
         if ans != prob:
             ceFound = True
             processCe(inp, ans, prob)
--- pythonStudy/Tools/CEMaker/inputGenerator.py
+++ pythonStudy/Tools/CEMaker/inputGenerator.py
@@ -1,3 +1,4 @@
+import random
 from random import randint
 from itertools import combinations
 
@@ -22,3 +23,25 @@ def p1107():
         for M in range(0, 10):
             for btn in combinations([str(i) for i in range(10)], M):
                 yield con(N, M, btn)
+
+def scpc2021_2():
+    for N in range(2, 15):
+        for t in range(1, N):
+            Bset = set()
+            for a in range(2**N):
+                A = bin(a)[2:2+N]
+                A = '0'*(N-len(A))+A
+                B = ['0']*N
+                for i in range(N):
+                    if (t<=i and A[i-t] == '1'): B[i] = '1'
+                    if (i<N-t and A[i+t] == '1'): B[i] = '1'
+                Bset.add(''.join(B))
+            inp = f'{len(Bset)}\n'
+            for b in Bset:
+                inp += f"{N} {t}\n{b}\n"
+            yield inp
+
+if __name__ == '__main__':
+    gen = scpc2021_2()
+    print(next(gen))
+    print(next(gen))
