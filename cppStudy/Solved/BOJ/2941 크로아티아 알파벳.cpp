85d1d4d93fa5581ee373e807d16d532d1b602240
heheHwang <hehehwang@gmail.com>
2021-07-20 15:55:08 +0900

    add 2941 크로아티아 알파벳.cpp

--- cppStudy/Solved/BOJ/2941 크로아티아 알파벳.cpp
+++ cppStudy/Solved/BOJ/2941 크로아티아 알파벳.cpp
@@ -9,8 +9,9 @@ int main(void) {
 
     int ans = 0;
     string croatian;
-    vector<string> toChange = {"c=", "c-", "d-", "lj", "nj", "s="};
     cin >> croatian;
+    vector<string> toChange = {"c=", "c-", "d-", "lj", "nj", "s=", "z="};
+    ans += int(croatian.size());
     for (auto c : toChange) {
         string::size_type n = croatian.find(c);
         while (n != string::npos) {
@@ -18,19 +19,10 @@ int main(void) {
             n = croatian.find(c, n + 1);
         }
     }
-    int c3 = 0, c2 = 0;
     string::size_type n = croatian.find("dz=");
     while (n != string::npos) {
-        c3++;
+        ans--;
         n = croatian.find("dz=", n + 1);
     }
-    n = croatian.find("z=");
-    while (n != string::npos) {
-        c2++;
-        n = croatian.find("z=", n + 1);
-    }
-    ans -= 2 * (c3);
-    ans -= (c2 - c3);
-    ans += int(croatian.size());
     cout << ans;
 }
