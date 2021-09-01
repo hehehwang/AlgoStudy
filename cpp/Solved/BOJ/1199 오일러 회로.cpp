17264f77736fab182c0f0ba572930c69228a8646
heheHwang <hehehwang@gmail.com>
2021-09-02 02:52:50 +0900

    update 1199 오일러 회로.cpp

--- cpp/Solved/BOJ/1199 오일러 회로.cpp
+++ cpp/Solved/BOJ/1199 오일러 회로.cpp
@@ -1,63 +1,48 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
+// 1199 오일러 회로
+// https://www.acmicpc.net/problem/1199
 using namespace std;
 
-int n;
-int adj[1001][1001] = { 0 };
-
-
-void getEurlarCircuit(int here, vector<int>& circuit)
-{
-	for (int there = 0; there < n; ++there)
-		while (adj[here][there] > 0)
-		{
-			adj[here][there]--;
-			adj[there][here]--;
-			getEurlarCircuit(there, circuit);
-		}
-	circuit.push_back(here + 1);
+int N, A[1010][1010], t;
+stack<pair<int, int>> edges[1010];
+vector<bool> chk;
+void printEularCircuit(int node) {
+    while (1) {
+        // 반대편에서 사용한 경우 감지
+        while (edges[node].size() && chk[edges[node].top().second])
+            edges[node].pop();
+        if (edges[node].empty()) break;
+        int nv, id;
+        tie(nv, id) = edges[node].top();
+        edges[node].pop();
+        chk[id] = 1;
+        printEularCircuit(nv);
+    }
+    cout << node << ' ';
 }
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	cin >> n;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			cin >> adj[i][j];
-		}
-	}
-
-	bool ok = true;
-	for (int i = 0; i < n; ++i)
-	{
-		int sum = 0;
-		for (int j = 0; j < n; ++j)
-		{
-			sum += adj[i][j];
-		}
-		if (sum % 2 == 1)
-		{
-			ok = false;
-			break;
-		}
-	}
-	vector<int> circuit;
-	if (ok)
-	{
-		getEurlarCircuit(0, circuit);
-		for (int i = 0; i < circuit.size(); i++)
-		{
-			cout << circuit[i] << ' ';
-		}
-	}
-	else
-	{
-		cout << -1;
-	}
+    cin >> N;
+    int id = 0;
+    for (int i = 1; i <= N; i++) {
+        int deg = 0;
+        for (int j = 1; j <= N; j++) {
+            cin >> t;
+            if (i < j) {
+                for (int k = 0; k < t; k++) {
+                    edges[i].push({j, ++id});
+                    edges[j].push({i, id});
+                }
+            }
+            deg += t;
+        }
+        if (deg % 2) {
+            cout << -1;
+            return 0;
+        }
+    }
+    chk.resize(id + 1);
+    printEularCircuit(1);
 }
