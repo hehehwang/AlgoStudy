72429063c455e8e5e701bb3b2b54636c3f91cea1
hwang h <hehehwang@gmail.com>
2022-01-03 13:14:15 +0900

    feature ce_finder

---
+++ ce_finder/finder.py
@@ -0,0 +1,57 @@
+import asyncio
+from input_generator import *
+
+
+async def runCmd(args, inp):
+    process = await asyncio.create_subprocess_exec(
+        *args, stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate(input=inp.encode())
+    return stdout.decode().strip()
+
+
+def processCe(inp, ans, prob):
+    print("!!!!Counter example found!!!!")
+    print("INPUT:")
+    print(inp)
+    print("ANSWER:")
+    print(ans)
+    print("WA:")
+    print(prob)
+
+
+def main(problem, answer, inputGenerator):
+    path = "ce_finder/target/"
+    ceFound = False
+    idx = 1
+    gen = inputGenerator()
+
+    problem, answer = [path + problem], [path + answer]
+    if problem[0].split(".")[-1] == "py":
+        problem = ["python"] + problem
+    if answer[0].split(".")[-1] == "py":
+        answer = ["python"] + answer
+    for inp in gen:
+        loop = asyncio.get_event_loop()
+        print("Case No.", idx)
+        commands = asyncio.gather(
+            runCmd(answer, inp), runCmd(problem, inp)  # ['python', './prob.py']
+        )
+        ans, prob = loop.run_until_complete(commands)
+        idx += 1
+        print("Input: ")
+        print(inp)
+        print("Answer: ")
+        print(ans)
+        print()
+        if ans != prob:
+            ceFound = True
+            processCe(inp, ans, prob)
+            loop.close()
+            break
+
+    print("NO CE FOUND T_T") if not ceFound else print("CE FOUND!")
+
+
+if __name__ != "__module__":
+    main("problem.py", "solution", p1107)
